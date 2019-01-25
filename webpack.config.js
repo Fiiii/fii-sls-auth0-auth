84ad99f219164c030989056cbe7eecc104aa819f
Fiiii <artur.figiel@netguru.co>
2019-01-25 23:57:41 +0100

    Drop webpack uglify module + resources update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack')
 const slsw = require('serverless-webpack')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -9,23 +9,27 @@ module.exports = {
   externals: [
     /aws-sdk/
   ],
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJsPlugin({
-      cache: true,
-      parallel: true,
-      uglifyOptions: {
-        compress: false,
-        ecma: 6,
-        mangle: true
-      },
-      sourceMap: true
-    })
-  ],
+  devtool: 'source-map',
+  optimization: {
+    minimize: false
+  },
+  // plugins: [
+  //   new webpack.LoaderOptionsPlugin({
+  //     minimize: true,
+  //     debug: false
+  //   }),
+  //   new webpack.optimize.ModuleConcatenationPlugin(),
+  //   new UglifyJsPlugin({
+  //     cache: true,
+  //     parallel: true,
+  //     uglifyOptions: {
+  //       compress: false,
+  //       ecma: 6,
+  //       mangle: true
+  //     },
+  //     sourceMap: true
+  //   })
+  // ],
   module: {
     rules: [
       {
